0acde4dfa7c87dc70f3d16b662876577ad87f76d
gwan.park <gwan.park@gmail.com>
2023-03-30 15:16:08 +0900

    commit for git pages

---
+++ src/routes/Home.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+const url =
+  "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(url);
+    const json = await response.json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>The Movies! {loading ? "" : `(${movies.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          {movies.map(movie => (
+            <Movie
+              key={movie.id}
+              coverImage={movie.medium_cover_image}
+              id={movie.id}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
